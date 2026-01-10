d819da4d5a60d76bd881082c5d5e4bfa93467367
Mohd. Abdul Ghani <marcusmars563@gmail.com>
2026-01-10 16:38:27 +0800

    fix

--- app/Models/AbsensiModel.php
+++ app/Models/AbsensiModel.php
@@ -75,6 +75,7 @@ class AbsensiModel extends Model
                                 guru.nama_lengkap as nama_guru,
                                 mata_pelajaran.nama_mapel,
                                 kelas.nama_kelas,
+                                jadwal_mengajar.kelas_id as kelas_id,
                                 jadwal_mengajar.hari')
             ->join('jadwal_mengajar', 'jadwal_mengajar.id = absensi.jadwal_mengajar_id')
             ->join('guru', 'guru.id = jadwal_mengajar.guru_id')
